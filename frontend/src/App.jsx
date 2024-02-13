04078e61d72cd64af67c15d6cfacea3350ed2dc3
Momin <mohammadmomin00@gmail.com>
2024-02-14 02:35:10 +0500

    create header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,22 @@ import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path={"/"} element={<Home />} />
-      <Route path={"/about"} element={<About />} />
-      <Route path={"/sign-in"} element={<SignIn />} />
-      <Route path={"/sign-up"} element={<SignUp />} />
-      <Route path={"/profile"} element={<Profile />} />
-    </Routes>
+    <>
+      {/*Header*/}
+      <Header />
+
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/sign-in"} element={<SignIn />} />
+        <Route path={"/sign-up"} element={<SignUp />} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className={"bg-slate-200 text-xl"}>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 px-12">
+        <Link to={"/"}>
+          <h1 className={"font-bold"}>Auth App</h1>
+        </Link>
+        <ul className={"flex gap-6"}>
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>SingIn</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
